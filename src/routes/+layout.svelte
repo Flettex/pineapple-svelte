c316d0e463321ba2a5630b3ec371f87e7df26f03
Rashaad Akbar <rushda2000@gmail.com>
2022-11-02 22:09:43 +0530

    Add half broken page load animation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
+  import RouteLoading from "$lib/components/routeLoading.svelte";
   import ToastGroup from "$lib/components/toast/toastGroup.svelte";
   import "../app.css";
 </script>
 
+<RouteLoading />
 <main class="min-h-screen">
   <slot />
 </main>
