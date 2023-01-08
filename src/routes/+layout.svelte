e35380c834836e8803037f448430ede63d50d339
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-01-07 21:24:30 -0500

    Mhm

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,33 @@
   import RouteLoading from "$lib/components/routeLoading.svelte";
   import SnackBarGroup from "$lib/components/snackbar/snackbarGroup.svelte";
   import "../styles/app.scss";
+
+  import { browser } from '$app/environment';
+
+  let darkMode = true;
+
+  function handleSwitchDarkMode() {
+      darkMode = !darkMode;
+
+      localStorage.setItem('theme', darkMode ? 'dark' : 'light');
+
+      darkMode
+          ? document.documentElement.classList.add('dark')
+          : document.documentElement.classList.remove('dark');
+  }
+
+  if (browser) {
+      if (
+          localStorage.theme === 'dark' ||
+          (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+      ) {
+          document.documentElement.classList.add('dark');
+          darkMode = true;
+      } else {
+          document.documentElement.classList.remove('dark');
+          darkMode = false;
+      }
+  }
 </script>
 
 <RouteLoading />
