2f25e297a78fe3dcd333e5328dd6e400385806f7
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-02-20 11:43:18 -0500

    useless

--- src/routes/chat/[guildID]/[channelID]/+page.server.ts
+++ src/routes/chat/[guildID]/[channelID]/+page.server.ts
@@ -1,7 +1,8 @@
+import { MAIN_CHANNEL } from "$lib/constants";
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, fetch }) => {
   return {
-    channel: await (await fetch(`/api/channels/${params.channelID}`)).json()
+    channel: (params.channelID === MAIN_CHANNEL.id ? MAIN_CHANNEL : await (await fetch(`/api/channels/${params.channelID}`)).json())
   };
 }
