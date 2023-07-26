7337127c155c12c74f63ee6c909bd52f1118af2e
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-07-25 22:46:53 -0400

    debugging in production

--- src/routes/chat/[guildID]/+layout.server.ts
+++ src/routes/chat/[guildID]/+layout.server.ts
@@ -2,6 +2,7 @@ import { MAIN_GUILD } from "$lib/constants";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (req) => {
+  console.log(req.params.guildId, MAIN_GUILD.id);
   if (req.params.guildId === MAIN_GUILD.id) {
     return { guild: MAIN_GUILD };
   }
--- src/routes/chat/[guildID]/[channelID]/+page.server.ts
+++ src/routes/chat/[guildID]/[channelID]/+page.server.ts
@@ -2,6 +2,7 @@ import { MAIN_CHANNEL } from "$lib/constants";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async (req) => {
+  console.log(req.params.guildId, MAIN_CHANNEL.id);
   if (req.params.channelID === MAIN_CHANNEL.id) {
     return { channel: MAIN_CHANNEL };
   }
