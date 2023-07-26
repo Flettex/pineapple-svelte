69109ef6ab0da3a63efe699437bae7eb59c827d0
Michaelsoft Binbows <61915438+NastyPigz@users.noreply.github.com>
2023-07-25 22:52:59 -0400

    debugging in production

--- src/routes/chat/[guildID]/+layout.server.ts
+++ src/routes/chat/[guildID]/+layout.server.ts
@@ -2,11 +2,11 @@ import { MAIN_GUILD } from "$lib/constants";
 import type { LayoutServerLoad } from "./$types";
 
 export const load: LayoutServerLoad = async (req) => {
-  console.log(req.params);
-  if (req.params.guildId === MAIN_GUILD.id) {
+  // console.log(req.params);
+  if (req.params.guildID === MAIN_GUILD.id) {
     return { guild: MAIN_GUILD };
   }
-  const resp = await fetch(`${req.url.protocol}//${req.url.host}/api/guilds/${req.params.guildId}`);
+  const resp = await fetch(`${req.url.protocol}//${req.url.host}/api/guilds/${req.params.guildID}`);
   // console.log("\n\nResponse: \n\n", JSON.parse(await resp.text()));
   console.log(resp);
   return {
